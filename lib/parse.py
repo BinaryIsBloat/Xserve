5fbca49f71a43ead4f82a114b58e90efc00b556b
RandomGuyWithoutY <95590184+RandomGuyWithoutY@users.noreply.github.com>
2022-08-08 19:15:07 +0200

    Requires further development

--- lib/parse.py
+++ lib/parse.py
@@ -1,10 +1,16 @@
-def parseheader(datastream):
-	datastream = datastream.splitlines()
-	line = datastream[0].split()
-	version = line[2].split(b"/")[1].split(b".")
-	request = {
-		"method": line[0],
-		"uri": line[1],
-		"version": tuple(int(version[0]), int(version[1]))
-	return 0 #returns very useful data
-	}
+# Specific parser exceptions
+class MalformedRequestTriplet(Exception):
+	pass
+
+def httpheader(stream):
+	line = stream.readline(0).split()
+	try:
+		head = {
+			"method": line[0],
+			"uri": line[1],
+			"version": line[2]
+		}
+	except IndexError:
+		raise MalformedRequestTriplet("The header did not contain the expected data triplet (method, uri, version)")
+	if head["method"] not in (b"GET", b"POST", b"HEAD", b"DELETE"):
+		pass
